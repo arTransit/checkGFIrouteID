0e48024491980c2dc8cbd5aec7594e8fe0716bec
Andrew Ross <andrew@fakePublicEmailAddress.ca>
2013-06-20 19:17:54 -0700

    Addressed case where GFI may not have bearing
    Initial GFI point will not have bearing NULL. Return stopid found by
    neighbourFinder.

--- matchBusStops.py
+++ matchBusStops.py
@@ -1,6 +1,6 @@
 import fmeobjects
 
-LOGIT=0
+LOGIT=1
 
 class FMEmatchBusStops(object):
     def __init__(self):
@@ -21,51 +21,57 @@ class FMEmatchBusStops(object):
         # Calculate the entry angle onto the bus as being 90 degrees left of the current bearing
         # Boarding angle is equal to the direction one would be facing standing on the sidewalk
         # facing the front door of the bus 
-        boardingAngle = (float(feature.getAttribute('BEARING')) -90) % 360
-        self.logging("FMEmatchBusStops: boardingAngle %s" % str(boardingAngle))
-        
-        # For each stop within the 60m radius defined by the neighbour finder determine the difference between the 
-        # Stop angle and the boarding angle.  0 indicates a perfect match
-        #divergence = map(lambda a: min( (float(a)-boardingAngle) % 360, (boardingAngle-float(a)) % 360), stopAngles)
-        divergence = dict(map( lambda (i,a): (i, min( (float(a)-boardingAngle) % 360, (boardingAngle-float(a)) % 360) ), 
-                stopAngles.items()))
-        
-        '''
-        for n in stopAngles:
-            if boardingAngle < 45 and float(n) > 315:
-                divergence.append(abs(float(boardingAngle)-float(n)+360))
-            elif boardingAngle > 315 and float(n) < 45:
-                divergence.append(abs(float(boardingAngle)-float(n)-360))
-            else:
-                divergence.append(abs(float(boardingAngle)-float(n)))
-        '''
-        self.logging("FMEmatchBusStops: checking %d stopAngles" % len(stopAngles))        
-        self.logging("FMEmatchBusStops: checking %d stopIDs" % len(stopIDs))        
-        self.logging("FMEmatchBusStops: checking %d distances" % len(distances))        
-        self.logging("FMEmatchBusStops: len.divergence %d" % len(divergence))        
-        
-        while len(distances) > 0:
-            minDistance = min(distances.values())
-            # minIndex = distances.index(minDistance)
-            minIndex = [k for k,v in distances.items() if v == minDistance][0]
-            if divergence[minIndex] < 100:
-                #self.logging("FMEmatchBusStops: trying to match: %s" % 'close{'+str(minIndex)+'}.STOPID')
-                match = stopIDs[minIndex] 
-                self.logging("FMEmatchBusStops: match %s" % str(match))
-                feature.setAttribute('MATCH',match)
-                self.logging("FMEmatchBusStops: matchDistance %s" % str(minDistance))
-                feature.setAttribute('MATCHDISTANCE',minDistance)
-                feature.setAttribute('STATUS','MATCHED')
-                feature.setAttribute('DIVERGENCE',divergence[minIndex])
-                break
-            else:
-                #distances.remove(minDistance)
-                del( distances[minIndex] )
-                
-        if len(distances) == 0:
-            feature.setAttribute('STATUS','TOODIVERGENT')
+        if feature.getAttribute('BEARING'):
+            boardingAngle = (float(feature.getAttribute('BEARING')) -90) % 360
+            self.logging("FMEmatchBusStops: boardingAngle %s" % str(boardingAngle))
+            
+            # For each stop within the 60m radius defined by the neighbour finder determine the difference between the 
+            # Stop angle and the boarding angle.  0 indicates a perfect match
+            #divergence = map(lambda a: min( (float(a)-boardingAngle) % 360, (boardingAngle-float(a)) % 360), stopAngles)
+            divergence = dict(map( lambda (i,a): (i, min( (float(a)-boardingAngle) % 360, 
+                    (boardingAngle-float(a)) % 360) ), stopAngles.items()))
+            
+            '''
+            for n in stopAngles:
+                if boardingAngle < 45 and float(n) > 315:
+                    divergence.append(abs(float(boardingAngle)-float(n)+360))
+                elif boardingAngle > 315 and float(n) < 45:
+                    divergence.append(abs(float(boardingAngle)-float(n)-360))
+                else:
+                    divergence.append(abs(float(boardingAngle)-float(n)))
+            '''
+            self.logging("FMEmatchBusStops: checking %d stopAngles" % len(stopAngles))        
+            self.logging("FMEmatchBusStops: checking %d stopIDs" % len(stopIDs))        
+            self.logging("FMEmatchBusStops: checking %d distances" % len(distances))        
+            self.logging("FMEmatchBusStops: len.divergence %d" % len(divergence))        
+            
+            while len(distances) > 0:
+                minDistance = min(distances.values())
+                # minIndex = distances.index(minDistance)
+                minIndex = [k for k,v in distances.items() if v == minDistance][0]
+                if divergence[minIndex] < 100:
+                    #self.logging("FMEmatchBusStops: trying to match: %s" % 'close{'+str(minIndex)+'}.STOPID')
+                    match = stopIDs[minIndex] 
+                    self.logging("FMEmatchBusStops: match %s" % str(match))
+                    feature.setAttribute('MATCH',match)
+                    self.logging("FMEmatchBusStops: matchDistance %s" % str(minDistance))
+                    feature.setAttribute('MATCHDISTANCE',minDistance)
+                    feature.setAttribute('STATUS','MATCHED')
+                    feature.setAttribute('DIVERGENCE',divergence[minIndex])
+                    break
+                else:
+                    #distances.remove(minDistance)
+                    del( distances[minIndex] )
+                    
+            if len(distances) == 0:
+                feature.setAttribute('STATUS','TOODIVERGENT')
 
-        feature.setAttribute('BANGLE',boardingAngle)
+            feature.setAttribute('BANGLE',boardingAngle)
+        else:
+            feature.setAttribute('STATUS','NEIGHBOURMATCHED')
+            feature.setAttribute('MATCH',feature.getAttribute('STOPID'))
+        
+        
         self.pyoutput(feature)
             
             
@@ -99,7 +105,7 @@ class FMEmatchBusStops(object):
         l = {}
         self.logging("getListAttribute: reading list %s" % s)
         for a in feature.getAllAttributeNames():
-            self.logging("getListAttribute: -- attribute %s" % a)
+            #self.logging("getListAttribute: -- attribute %s" % a)
             if a.startswith( s[:s.find('{')] ) and a.endswith( s[s.find('}'):] ):
                 i = int( a[a.find('{') + 1 : a.find('}')] )
                 l[i] = str(feature.getAttribute( a ))
